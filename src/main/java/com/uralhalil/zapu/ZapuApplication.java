46bd8e60f7a43289adc0637c13103f4bd3029cb9
halilural <halilural5@gmail.com>
2021-08-31 22:40:42 +0300

    first commit
    first commit

---
+++ src/main/java/com/uralhalil/zapu/ZapuApplication.java
@@ -0,0 +1,13 @@
+package com.uralhalil.zapu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZapuApplication.class, args);
+    }
+
+}
